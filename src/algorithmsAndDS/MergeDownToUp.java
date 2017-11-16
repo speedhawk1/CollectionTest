31d7c5432487b2029ed2685c60ff60043d003b3c
speedhawk1 <1010259701@qq.com>
2017-11-16 23:23:48 +0800

    change files

--- src/algorithmsAndDS/MergeDownToUp.java
+++ src/algorithmsAndDS/MergeDownToUp.java
@@ -11,7 +11,10 @@ public class MergeDownToUp {
 
         int N = a.length;
         aux = new Comparable[N];   // 确定数组空间
-        for (int i = 0; i < N; i += i) {  // 确定每一层的数组大小i;
+        for (int len = 1; len < N; len += len) {// 确定每一层的数组大小len;
+            for (int lo = 0; lo < N - len; lo++) { // 建立索引，并对每2个子数组两两归并排序。
+                Merge.merge(a,lo,lo+len-1,Math.min(lo+len-1+len,N));
+            }
         }
     }
 }
